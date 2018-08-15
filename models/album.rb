8f00396091b72ba4d6420eb5a21def5179c6bd6a
Fofiko <foteinid@gmail>
2018-08-15 22:18:32 +0100

    Added save methods for Artist and Album

--- models/album.rb
+++ models/album.rb
@@ -13,5 +13,15 @@ class Album
     @artist_id = info["artist_id"].to_i
   end
 
+  def save()
+    sql = "INSERT INTO albums
+    (title, genre, artist_id)
+    VALUES
+    ($1, $2, $3)
+    RETURNING *"
+    values = [@title, @genre, @artist_id]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
 
 end
--- models/artist.rb
+++ models/artist.rb
@@ -11,5 +11,16 @@ class Artist
     @name = info["name"]
   end
 
+  def save()
+    sql = "INSERT INTO artists
+    (name)
+    VALUES
+    ($1)
+    RETURNING *"
+    values = [@name]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+
 
 end
